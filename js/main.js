7c15a236d3c069355a2ac95301f040941cf64e74
nardo88 <nardo1988@mail.ru>
2021-02-08 21:16:29 +0300

    media header

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const menu = () =>{
+        const header = document.querySelector('.header');
+        const burger = document.querySelector('.burger');
+        const headerNav = document.querySelector('.header__nav');
+        const headerForm = document.querySelector('.header__form');
+        const headerLogo = document.querySelector('.header__logo');
+        const headerCartLink = document.querySelector('.header__cart-link');
+
+        const toggleMenu = () => {
+            burger.classList.toggle('open-burger');
+            headerNav.classList.toggle('open-menu');
+
+        }
+
+        const toggleFind = () => {
+            headerForm.classList.toggle('open-find');
+            headerLogo.classList.toggle('hide');
+            headerCartLink.classList.toggle('hide-cart');
+            
+        }
+
+
+        header.addEventListener('click', e => {
+            const target = e.target;
+            
+            if (target.closest('.burger')){
+                toggleMenu();
+
+            }
+
+            if (target.classList.contains('header__mobile-btn')){
+                toggleFind();
+            }
+        })
+    }
+
+    menu();
+
+
+
+
+})
